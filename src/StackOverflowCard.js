57d55ac3c73449884659c33fb4142b057ac92c09
Omid Nikrah <omidnikrah@gmail.com>
2020-07-30 02:35:13 +0430

    change card width

--- src/StackOverflowCard.js
+++ src/StackOverflowCard.js
@@ -5,7 +5,7 @@ const ArticleCard = async (data) => {
   const namePosition = (210 - (data.display_name.length * 8.5)) / 2;
 
   return `
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="270.933mm" height="82.55mm" version="1.2" baseProfile="tiny">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="210px" height="82.55mm" version="1.2" baseProfile="tiny">
       <g fill="none" stroke="black" stroke-width="1" fill-rule="evenodd" stroke-linecap="square" stroke-linejoin="bevel">
         <g fill="#000000" fill-opacity="1" stroke="#000000" stroke-opacity="1" stroke-width="1" stroke-linecap="square" stroke-linejoin="miter" stroke-miterlimit="2" transform="matrix(1,0,0,1,0,0)"></g>
         <g fill="#4a4e51" fill-opacity="1" stroke="none" transform="matrix(1,0,0,1,0,0)">
