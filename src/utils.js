abcba0c774e77adf2985394e16ed8f3c4322fcca
Omid Nikrah <omidnikrah@gmail.com>
2026-09-24 00:06:58 +0330

    fix: fix stackoverflow profile_image issue

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
 const fetch = require('node-fetch');
 
-const imgToDataURL = url => {
-  return fetch(url).then(response => response.buffer()).then(buffer => `data:image/png;base64,${buffer.toString('base64')}`);
+const imgToDataURL = async url => {
+  const response = await fetch(url, { headers: { Referer: 'https://stackoverflow.com/' } });
+  const contentType = response.headers.get('content-type') || 'image/png';
+  const buffer = await response.buffer();
+  return `data:${contentType};base64,${buffer.toString('base64')}`;
 };
 
 module.exports = { imgToDataURL };
